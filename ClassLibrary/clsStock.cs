fd9948908d55420590ebe24455af73151572ec8e
rstep67 <rstephenson67@outlook.com>
2024-05-07 12:14:13 +0100

    made testing classes

--- ClassLibrary/clsStock.cs
+++ ClassLibrary/clsStock.cs
@@ -1,6 +1,15 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsStock
     {
+        public bool Active { get; set; }
+        public DateTime DateAdded { get; set; }
+        public string Description { get; set; }
+        public int ItemPrice { get; set; }
+        public string ItemName { get; set; }
+        public int SupplierId { get; set; }
+        public int ItemId { get; set; }
     }
 }
--- Testing3/tstStock.cs
+++ Testing3/tstStock.cs
@@ -12,6 +12,101 @@ namespace Testing3
         {
             clsStock aStock = new clsStock();
             Assert.IsNotNull(aStock);        
+        } 
+        
+        //itemid int,
+        //
+        //
+        //supplierid int,
+        //
+        //
+        //itemname char,
+          //
+          //
+          //
+          //itemprice int,
+          //
+          //
+          //
+          //itemdescription string,
+          //
+          //
+          //
+          //
+          //dateofdelivery date
+
+        [TestMethod] //sees if stock is active
+        public void ActiveStockOK()
+        {
+            //create instance of class
+            clsStock aStock = new clsStock();
+            //create test data to assign to property
+            Boolean testStock = true;
+            //assin the data to the property
+            aStock.Active = testStock;
+            //test to see that the two values are the same
+            Assert.AreEqual(aStock.Active, testStock);
         }
+
+        [TestMethod]  //date stock added
+        public void DateAddedStockOK() 
+        {  
+            clsStock aStock = new clsStock();
+            DateTime TestData = DateTime.Now.Date;
+            aStock.DateAdded = TestData;
+            Assert.AreEqual(aStock.DateAdded, TestData);
+        
+        }
+
+
+        [TestMethod]
+        public void ItemDescriptionOK() 
+        {
+            clsStock aStock = new clsStock();
+            string TestData = "Some description";
+            aStock.Description = TestData;
+            Assert.AreEqual(aStock.Description, TestData);
+
+
+        }
+
+        [TestMethod]
+        public void ItemPriceOK() 
+        {
+            clsStock aStock = new clsStock();
+            Int32 TestData = 1;
+            aStock.ItemPrice = TestData;
+            Assert.AreEqual(aStock.ItemPrice, TestData);
+        
+        }
+
+        [TestMethod]
+        public void ItemNameOK() 
+        {
+            clsStock aStock = new clsStock();
+            string TestData = "Some name";
+            aStock.ItemName = TestData;
+            Assert.AreEqual(aStock.ItemName, TestData);
+        
+        }
+
+        [TestMethod]
+        public void SupplierIdOK()
+        {
+            clsStock aStock = new clsStock();
+            Int32 TestData = 1;
+            aStock.SupplierId = TestData;
+            Assert.AreEqual(aStock.SupplierId, TestData);
+        }
+
+        [TestMethod]
+        public void ItemIdOK()
+        {
+            clsStock aStock = new clsStock();
+            Int32 TestData = 1;
+            aStock.ItemId = TestData;
+            Assert.AreEqual(aStock.ItemId, TestData);
+        }
+
     }
 }
