8294ef1d9ed104265d28e1fbac3429e56259b2b1
p2657437 <p2657437@GWHXX284WIN014W>
2024-05-07 22:25:31 +0100

    added testing for supplier class properties

--- ClassLibrary/clsSupplier.cs
+++ ClassLibrary/clsSupplier.cs
@@ -1,6 +1,15 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsSupplier
     {
+        public int SupplierID { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Email { get; set; }
+        public string TelephoneNumber { get; set; }
+        public DateTime AddDate { get; set; }
+        public bool Availability { get; set; }
     }
 }
