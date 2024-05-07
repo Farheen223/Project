ae07c5a72420eeaaefdc24b92cf720c273a66a64
Gradi <p2644927@GWHXX284WIN007W>
2024-05-07 12:25:52 +0100

    Testing property

---
+++ ClassLibrary/clsOrder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Testing6
+{
+    public class clsOrder
+    {
+        public bool Active { get; set; }
+        public DateTime DateAdded { get; set; }
+        public string Street { get; set; }
+        public string Town { get; set; }
+        public string PostCode { get; set; }
+        public string HouseNo { get; set; }
+        public int CountyCode { get; set; }
+        public int OrderId { get; set; }
+    }
+}
--- Testing6/tstOrder.cs
+++ Testing6/tstOrder.cs
@@ -14,5 +14,147 @@ namespace Testing6
             //test to see that it exists
             Assert.IsNotNull(AnOrder);
         }
+
+        [TestMethod]
+        public void ActivePropertyOK()
+        //create an instance of the class we want to create
+        {
+            clsOrder AnOrder = new clsOrder();
+            //create some test data to assaign to the property
+            Boolean TestData = true;
+            //assign the data to the property
+            AnOrder.Active = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnOrder.Active, TestData);
+
+        }
+        [TestMethod]
+        public void DateAddedPropertyOK()
+        {
+
+            //create an instance of the class we want to create
+            clsOrder AnOrder = new clsOrder();
+            //create some test data to assign to the property
+            DateTime TestData = DateTime.Now.Date;
+            //assign the data to the property
+            AnOrder.DateAdded = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnOrder.DateAdded, TestData);
+        }
+    }
+
+    namespace Testing6
+    {
+        [TestClass]
+        public class tstAddress
+        {
+            [TestMethod]
+            public void InstanceOK()
+            {
+                //create an instance of the class we want to create
+                clsOrder AnAddress = new clsOrder();
+                //test to see that it exists
+                Assert.IsNotNull(AnAddress);
+            }
+            [TestMethod]
+            public void ActivePropertyOK()
+            {
+                //create an instance of the class we want to create
+                clsOrder AnAddress = new clsOrder();
+                //create some test data to assign to the property
+                Boolean TestData = true;
+                //assign the data to the property
+                AnAddress.Active = TestData;
+                //test to see that the two values are the same
+                Assert.AreEqual(AnAddress.Active, TestData);
+            }
+            [TestMethod]
+            public void DateAddedPropertyOK()
+            {
+                //create an instance of the class we want to create
+                clsOrder AnAddress = new clsOrder();
+                //create some test data to assign to the property
+                DateTime TestData = DateTime.Now.Date;
+                //assign the data to the property
+                AnAddress.DateAdded = TestData;
+                //test to see that the two values are the same
+                Assert.AreEqual(AnAddress.DateAdded, TestData);
+            }
+            [TestMethod]
+            public void AddressIdPropertyOK()
+            {
+                //create an instance of the class we want to create
+                clsOrder AnOrder = new clsOrder();
+                //create some test data to assign to the property
+                Int32 TestData = 1;
+                //assign the data to the property
+                AnOrder.OrderId = TestData;
+                //test to see that the two values are the same
+                Assert.AreEqual(AnOrder.OrderId, TestData);
+            }
+
+            [TestMethod]
+            public void CountyCodePropertyOK()
+            {
+                //create an instance of the class we want to create
+                clsOrder AnOrder = new clsOrder();
+                //create some test data to assign to the property
+                Int32 TestData = 1;
+                //assign the data to the property
+                AnOrder.CountyCode = TestData;
+                //test to see that the two values are the same
+                Assert.AreEqual(AnOrder.CountyCode, TestData);
+            }
+            [TestMethod]
+            public void HouseNoPropertyOK()
+            {
+                //create an instance of the class we want to create
+                clsOrder AnOrder = new clsOrder();
+                //create some test data to assign to the property
+                string TestData = "21b";
+                //assign the data to the property
+                AnOrder.HouseNo = TestData;
+                //test to see that the two values are the same
+                Assert.AreEqual(AnOrder.HouseNo, TestData);
+            }
+            [TestMethod]
+            public void PostCodePropertyOK()
+            {
+                //create an instance of the class we want to create
+                clsOrder AnOrder = new clsOrder();
+                //create some test data to assign to the property
+                string TestData = "LE1 4AB";
+                //assign the data to the property
+                AnOrder.PostCode = TestData;
+                //test to see that the two values are the same
+                Assert.AreEqual(AnOrder.PostCode, TestData);
+            }
+            [TestMethod]
+            public void StreetPropertyOK()
+            {
+                //create an instance of the class we want to create
+                clsOrder AnOrder = new clsOrder();
+                //create some test data to assign to the property
+                string TestData = "Some Street";
+                //assign the data to the property
+                AnOrder.Street = TestData;
+                //test to see that the two values are the same
+                Assert.AreEqual(AnOrder.Street, TestData);
+            }
+
+            [TestMethod]
+            public void TownPropertyOK()
+            {
+                //create an instance of the class we want to create
+                clsOrder AnAddress = new clsOrder();
+                //create some test data to assign to the property
+                string TestData = "Leicester";
+                //assign the data to the property
+                AnAddress.Town = TestData;
+                //test to see that the two values are the same
+                Assert.AreEqual(AnAddress.Town, TestData);
+
+            }
+        }
     }
 }
