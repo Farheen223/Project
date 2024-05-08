1d80de1872aa0a39784f6731522589cee948488b
Tommy <p2774538@GWHXX284WIN013W>
2024-05-08 11:33:12 +0100

    Added properties for staff

--- ClassLibrary/clsStaff.cs
+++ ClassLibrary/clsStaff.cs
@@ -1,6 +1,15 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsStaff
     {
+        public bool Active { get; set; }
+        public DateTime DateAdded { get; set; }
+        public int StaffId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string TelephoneNumber { get; set; }
+        public string Hours { get; set; }
     }
 }
