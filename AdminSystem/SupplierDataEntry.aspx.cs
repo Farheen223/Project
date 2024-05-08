85643829af4083af81f3bcd5d950a08abdff5171
p2657437 <p2657437@GWHXX284WIN014W>
2024-05-08 18:18:20 +0100

    created interface for supplier data entry page

--- AdminSystem/SupplierDataEntry.aspx.cs
+++ AdminSystem/SupplierDataEntry.aspx.cs
@@ -15,6 +15,22 @@ public partial class _1_DataEntry : System.Web.UI.Page
 
     protected void btnOK_Click(object sender, EventArgs e)
     {
+        // create a new instance of clsSupplier
+        clsSupplier supplier = new clsSupplier();
+        // capture the name
+        supplier.Name = txtSupplierName.Text;
+        // capture the city
+        supplier.City = txtSupplierCity.Text;
+        // capture the email
+        supplier.Email = txtSupplierEmail.Text;
+        // capture the telephone number
+        supplier.TelephoneNumber = txtSupplierTelephoneNumber.Text;
+        // capture availability check box
+        supplier.Availability = chkSupplierAvailability.Checked;
+        // capture add date
+        supplier.AddDate = Convert.ToDateTime(DateTime.Now);
+        // store the supplier info in the session object
+        Session["supplier"] = supplier;
         // navigate to the supplier view page
         Response.Redirect("SupplierViewer.aspx");
     }
--- AdminSystem/SupplierViewer.aspx
+++ AdminSystem/SupplierViewer.aspx
@@ -7,7 +7,6 @@
     <title></title>
 </head>
 <body>
-    This is the supplier view page
     <form id="form1" runat="server">
         <div>
         </div>
