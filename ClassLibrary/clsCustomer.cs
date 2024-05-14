bb1bb29b69e594d94b957ea379b0aea85b917425
Farheen <p2767887@GWHXX284WIN015W>
2024-05-14 17:05:00 +0100

    Customer

--- ClassLibrary/clsCustomer.cs
+++ ClassLibrary/clsCustomer.cs
@@ -4,17 +4,11 @@ namespace ClassLibrary
 {
     public class clsCustomer
     {
-        public clsCustomer()
-        {
-        }
-
         public bool Active { get; set; }
-        public DateTime DateAdded { get; set; }
-        public int CustomerId { get; set; }
-        public int CountyCode { get; set; }
-        public string HouseNo { get; set; }
-        public string PostCode { get; set; }
-        public string Street { get; set; }
-        public string Town { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public string Email { get; set; }
+        
     }
 }
