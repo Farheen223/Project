bf390a564902730e5312ca50f3941c8ec24f7b8f
Gradi <p2644927@GWHXX284WIN007W>
2024-05-23 01:28:57 +0100

    continuing with prac 9b still alot of errors

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -3,8 +3,8 @@ GO
 
 DECLARE	@return_value Int
 
-EXEC	@return_value = [dbo].[sproc_tblCustomer_FilterByCustomerId]
-		@CustomerId = 1
+EXEC	@return_value = [dbo].[sproc_tblOrder_FilterByOrderId]
+		@OrderId = 1
 
 SELECT	@return_value as 'Return Value'
 
