98988b00191e14ef76d5501a016dafc530b2f635
Farheen <p2767887@GWHXX284WIN015W>
2024-05-29 13:05:06 +0100

    on practical 15

---
+++ AdminSystem/CustomerLogin.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="CustomerLogin.aspx.cs" Inherits="CustomerLogin" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Customer Login Page </title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+    <p>
+        Customer Login Page</p>
+        <asp:Label ID="lblUserName" runat="server" Text="UserName"></asp:Label>
+        <asp:TextBox ID="txtUserName" runat="server" style="margin-left: 36px"></asp:TextBox>
+        <p>
+            <asp:Label ID="lblPassword" runat="server" Text="Password"></asp:Label>
+            <asp:TextBox ID="txtPassword" runat="server" style="margin-left: 39px" TextMode="Password" Width="123px"></asp:TextBox>
+            <asp:Label ID="lblError" runat="server" ForeColor="#FF3300"></asp:Label>
+        </p>
+        <asp:Button ID="btnLogin" runat="server" Height="25px" OnClick="btnLogin_Click" style="margin-left: 52px" Text="Login" Width="52px" />
+        <asp:Button ID="btnCancel" runat="server" Height="25px" style="margin-left: 44px" Text="Cancel" Width="56px" />
+    </form>
+    </body>
+</html>
--- ClassLibrary/clsCustomerUser.cs
+++ ClassLibrary/clsCustomerUser.cs
@@ -5,14 +5,76 @@ namespace ClassLibrary
 {
     public class clsCustomerUser
     {
-        public int UserID { get; set; }
-        public string UserName { get; set; }
-        public string Password { get; set; }
-        public string Department { get; set; }
+        private Int32 mUserID;
+        private String mUserName;
+        private String mPassword;
+        private String mDepartment;
 
-        public bool FindUser(string userName, string password)
+        public int UserID 
         {
-            throw new NotImplementedException();
+            get
+            {
+                return mUserID;
+            }
+            set
+            {
+               mUserID = value;
+            }
+        }
+        public string UserName 
+        { 
+            get
+            {
+                return mUserName;
+            }
+            set
+            {
+                mUserName = value;
+            }
+        }
+
+
+        public string Password 
+        { get
+            {
+                return mPassword;
+            }
+            
+          set
+            {
+                mPassword = value;
+            }
+        }
+        public string Department 
+        { 
+            get
+            {
+                return mDepartment;
+            }
+            set
+            {
+                mDepartment = value;
+            }
+        }
+
+        public bool FindUser(string UserName, string Password)
+        {
+            clsDataConnection DB = new clsDataConnection();
+            DB.AddParameter("@UserName", UserName);
+            DB.AddParameter("@Password", Password);
+            DB.Execute("sproc_tblUsers_FindUserNameP1");
+            if (DB.Count == 1)
+            {
+                mUserID = Convert.ToInt32(DB.DataTable.Rows[0]["UserID"]);
+                mUserName = Convert.ToString(DB.DataTable.Rows[0]["UserName"]);
+                mPassword = Convert.ToString(DB.DataTable.Rows[0]["Password"]);
+                mDepartment = Convert.ToString(DB.DataTable.Rows[0]["Department"]);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
